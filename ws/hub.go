01ec0ecbd3a519fd73b9b2fcfded95db96005980
evpeople <hangcaihui@gmail.com>
2022-03-16 23:12:35 +0800

    ✨ feat(hub.go): change welcome
    welcome message become json
    
    #1

--- ws/hub.go
+++ ws/hub.go
@@ -39,6 +39,19 @@ func (h *Hub) Run() {
 			}
 		case message := <-h.broadcast:
 			//从 broadcast获取了消息后，在每一个当前存在的链接上发送消息。
+
+			//TODO issue #1 ,此处应该序列化json 数据
+			/*{
+				"to":{"evpeople","verso"},
+				"dialog":"聊天大厅"
+				"type":"text",
+				"text":"xxxxxxxxxxxxx"
+			}*/
+			//然后从json中提取出 to 的信息。
+			//client 有usr的信息，所以可以与 to 的内容做匹配，从而实现发送到指定的人。
+			//如果比较闲，可以给报文增加一个"timeStamp":时间戳的项。
+			//另规定：当头部中"to":{}时发送到所有连接到服务器的用户。
+			//
 			for client := range h.clients {
 				select {
 				//这个send的意思是发送给当前存在的连接上。
